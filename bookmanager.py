195237a94640c43dc829c83f0a2bc2076ee54f07
Kelso du Mez <17232@burnside.school.nz>
2020-05-19 14:18:26 +1200

    fixed git

--- bookmanager.py
+++ bookmanager.py
@@ -1,18 +1,15 @@
 import os
-
 from flask import Flask 
 from flask  import render_template
 from flask import request
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
 
-
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 
 db = SQLAlchemy(app)
 
@@ -47,7 +44,6 @@ def home():
     books = Book.query.all()
     films = Film.query.all()
     return render_template("home.html", books=books, films=films)
-
 
 @app.route("/update", methods=["POST"])
 def update():
